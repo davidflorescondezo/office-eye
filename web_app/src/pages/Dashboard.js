2e37cdd5a505faf4054938994b86c896b91aed76
David Flores <u6051419@anu.edu.au>
2019-02-22 02:17:17 +1100

    Finshed Dashboard Graphs & recreated the arduino to match with the wifi shield

--- web_app/src/pages/Dashboard.js
+++ web_app/src/pages/Dashboard.js
@@ -11,6 +11,8 @@ import * as routes from "../router/routes";
 import {auth,database,storage} from '../firebase/firebase';
 
 import Graph from "../account/graph"
+import CircularProgressbar from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 const { Sider, Content, Header} = Layout;
 class Dashboard extends Component {
@@ -65,11 +67,21 @@ class Dashboard extends Component {
         //graphRef
         let _this = this;
         //if(this.state.authUserDir && (this.state.judgeEnrollment != 0)) {
-            let postsRef1 = database.ref('/arduinoData').orderByValue().limitToLast(1);
+            //let postsRef1 = database.ref('/arduinoData').orderByValue().limitToLast(1);//ONLY FOR ETHERNET
+            let postsRef1 = database.ref('/arduinoData').orderByChild("value").limitToLast(1);
+            console.log(postsRef1);
 
-            postsRef1.on('value', (snapshot) => {
-                this.setState({graphRef: snapshot.val()})
-            });
+            postsRef1.on("value", function(snapshot) {
+                snapshot.forEach(function(child) {
+                  console.log(child.val().value);
+                  _this.setState({graphRef: child.val()});
+                });
+              });
+
+            /*postsRef1.on('value', (snapshot) => {
+                this.setState({graphRef: snapshot.val().value})
+                console.log(snapshot.val().value);
+            });*/ //ONLY FOR ETHERNET
        // }
     }
 
@@ -105,15 +117,61 @@ class Dashboard extends Component {
             }
         ]
 
+        if((Math.floor((Math.floor((graphList)/60))/60)) == 0){
+            var hoursLeft = 0;
+        }
+        else 
+            var hoursLeft = (8/(Math.floor((Math.floor((graphList)/60))/60)))*100;
+        
+        var percentage = hoursLeft
+
         return(
             this.state.authUser
                 ?(
-                    <div>
-                    <Headers authUser={this.props.authUser}/>
-                    <Graph
-                        times={fullData}
-                        graphTitle="Current Monitoring"
-                    />
+                    <div className="boxContainer">
+                        <Headers authUser={this.props.authUser}/>
+                        <div className="selfContainer">
+                        <h2>Current Employee Working Time <span className="dateStyle">{Date()}</span></h2>
+                        <div className="tableBStyle">
+                        <table>
+                            <tr>
+                                <th><h3>&nbsp;<Icon type="clock-circle" />&nbsp;Total hours spent working</h3></th>
+                                <th><h3>&nbsp;<Icon type="clock-circle" />&nbsp;Total minutes spent working</h3></th>
+                                <th><h3>&nbsp;<Icon type="clock-circle" />&nbsp;Total seconds spent working</h3></th>
+                            </tr>
+                            <tr>
+                                <td className="tableStyle" width="35%"><h1>&nbsp; {Math.floor((Math.floor((graphList)/60))/60)/*HOURS*/}</h1></td>
+                                <td className="tableStyle" width="35%"><h1>&nbsp; {Math.floor((graphList)/60)/*MINUTES*/}</h1></td> 
+                                <td width="40%"><h1>&nbsp; {graphList/*SECONDS*/}</h1></td>
+                                <td></td>
+                            </tr>
+                        </table>
+                        </div>
+                    </div>
+                    <div className="graphBg">
+                    <table>
+                        <tr>
+                            <td width="90%">
+                                <Graph
+                                    times={fullData}
+                                    graphTitle="Current Monitoring"
+                                />
+                            </td>
+                            <td width="10%">
+                                <div className="circleGraph">
+                                <CircularProgressbar
+                                    percentage={percentage}
+                                    text={`${percentage}%`}
+                                    styles={{
+                                        path: { stroke: `rgba(0, 153, 51, ${percentage / 100})` },
+                                        text: { fill: '#003300', fontSize: '26px' },
+                                    }}
+                                /> <div className="circleFont"> Hours left to finish work day </div>
+                                </div>
+                            </td>
+                        </tr>
+                    </table>
+                    </div>
                     </div>
                     /*<Layout>
                         <Route  render={(props) => (
